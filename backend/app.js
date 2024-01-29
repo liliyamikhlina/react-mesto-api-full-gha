7d4a44f1c3be1501c20fd242c98242fff3bfa7a0
Liliya Mikhlina <liliya.mikhlina@yandex.ru>
2024-01-29 03:22:45 +0200

    fix: mongodb settings

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,10 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(requestLogger);
 
